fe4c727815292720117b2c8d0c452dd36b33e150
Mansour.vn <67545478+Mansour-VN@users.noreply.github.com>
2022-12-06 16:17:07 +0330

    update 3 files, create 23 files and delete 9 files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from 'components/index';
+import {Share} from './assets/image/index';
+import './assets/style/App.css';
+import './assets/style/index.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
